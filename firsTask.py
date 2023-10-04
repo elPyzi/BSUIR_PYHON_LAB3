24e4bdc2c25624b6193a437ba46b108fc460f0bb
Danila Pyzhikov <danila.pyzhikov@gmail.com>
2023-10-04 18:51:44 +0300

    Правки в задании

--- firsTask.py
+++ firsTask.py
@@ -1,15 +1,3 @@
-# with open("F1.txt", "w") as f1:
-#     while True:
-#         line = input("Введите строку данных (пустая строка для заdhfgfвершения): ")
-#         if not line:
-#             break
-#         f1.write(line + "\n")
-# with open("F1.txt", "r") as f1, open("F2.txt", "w") as f2:
-#     for line in f1:
-#         cleaned_line = line.strip()
-#         if cleaned_line.endswith("А") or cleaned_line.endswith("а"):
-#             f2.write(cleaned_line + "\n")
-# print("Программа завершена. Строки, заканчивающиеся на 'А', скопированы в файл F2.")
 
 
 with open("F1.txt", "w") as f1:
